83201ffc02487c03769ef6021d898a085687c0e7
Wille2 <villeahti@hotmail.com>
2013-02-16 15:38:13 +0200

    Controlable commented

--- src/org/wiltzu/ui/component/Controlable.java
+++ src/org/wiltzu/ui/component/Controlable.java
@@ -1,4 +1,3 @@
-
 package org.wiltzu.ui.component;
 
 import java.awt.Rectangle;
@@ -9,23 +8,60 @@ import org.wiltzu.ui.component.exception.CollisionException;
 import org.wiltzu.util.Direction;
 
 /**
+ *<p>Interface for Drawable classes that can be controlled and their collisions can be checked.</p>
  *
- * @author Ville
+ * @author Ville Ahti
  */
 public interface Controlable extends Drawable{
 	
+	/**
+	 * <p>Setter for x coordinate.</p>
+	 * 
+	 * @param x coordinate
+	 */
 	void setX(int x);
 
+    /**
+     * <p>Setter for y coordinate.</p>
+     * 
+     * @param y cooredinate
+     */
     void setY(int y);
     
+    /**
+     * <p>Getter for previous bounds.</p>
+     * 
+     * @return previous bounds
+     */
     Rectangle getLastBounds();
 
+    /**
+     * <p>Getter for current bounds.</p>
+     * 
+     * @return current bounds
+     */
     Direction getDirection();
     
+    /**
+     * <p>Getter for previous direction</p>
+     * 
+     * @return previous direction
+     */
     Direction getLastDirection();
 
+    /**
+     * <p>Setter for new direction.</p>
+     * 
+     * @param dir to set
+     */
     void setDirection(Direction dir);
     
+    /**
+     * <p>for updating status of the class and checking collisions</p>
+     * 
+     * @param container
+     * @throws CollisionException
+     */
     void update(JPanel container) throws CollisionException;
     
 }
